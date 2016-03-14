f06e1a9c247f7314b80e7cd350c277a94f53aace
Diego Martinez <diegomart_2000@yahoo.com>
2016-03-13 21:40:43 -0300

    First Impl

---
+++ mobile/www/js/app/model/codes.model.js
@@ -0,0 +1,21 @@
+/**
+ * CODES and STATUSES constants
+ */
+define(function(){
+	return {
+		CALLTYPES: {
+			POLICE: 1,
+			FIREFIGHTERS: 2,
+			AMBULANCE: 3,
+			VIOLENCE: 4
+		},
+
+		CALLTYPES_BY_CODE: {
+			1: 'POLICE',
+			2: 'FIREFIGHTERS',
+			3: 'AMBULANCE',
+			4: 'VIOLENCE'
+		}
+
+	};
+});
--- mobile/www/js/index.js
+++ mobile/www/js/index.js
@@ -1,58 +1,29 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
+// For any third party dependencies, like jQuery, place them in the lib folder.
 
-         // Register the event listener
+// Configure loading modules from the lib directory,
+// except for 'app' ones, which are in a sibling
+// directory.
+requirejs.config({
+    baseUrl: 'js/libs',
+    paths: {
+        app: '../app'
     },
 
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.addEventListener("volumedownbutton", this.onVolumeDownKeyDown, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
+    shim: {
+    	underscore: {
+    		exports: '_'
+    	},
 
-    onVolumeDownKeyDown: function(){
-        app.receivedEvent('call-emergency');
-    },
-    
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    	handlebars: {
+    		exports: 'Handlebars'
+    	}
     }
-};
+});
+
+// Start loading the main app file. Put all of
+// your application logic in there.
+requirejs(['./app/boton.app'], function(ButtonApp){
+	console.log('INDEX App initialization...');
+
+	ButtonApp.initialize();
+});
